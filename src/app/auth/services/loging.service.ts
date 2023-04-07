cb4edff2df9ff866de0215639991068be1463a37
SAK74 <sergijks@gmail.com>
2023-04-07 18:51:58 +0200

    feat(login): add logged observable

--- src/app/auth/services/loging.service.ts
+++ src/app/auth/services/loging.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 
 const STORAGEKEY = 'you2Be-token';
 const fakeToken = 'faketokenexampled';
@@ -9,18 +10,22 @@ const initialName = 'Your Name';
 export class LoginService {
   constructor(private router: Router) {
     this.isLogged = Boolean(window.localStorage.getItem(STORAGEKEY));
+    this.loggedObserver.next(this.isLogged);
   }
   isLogged: boolean;
   user = initialName;
+  loggedObserver = new Subject<boolean>();
   logIn(user: string) {
     window.localStorage.setItem(STORAGEKEY, fakeToken);
     this.isLogged = true;
     this.user = user;
+    this.loggedObserver.next(true);
     this.router.navigateByUrl('youtube');
   }
   logOut() {
     this.isLogged = false;
     this.user = initialName;
+    this.loggedObserver.next(false);
     this.router.navigateByUrl('auth');
     window.localStorage.removeItem(STORAGEKEY);
   }
--- src/app/core/components/header/user-block/user-block.component.ts
+++ src/app/core/components/header/user-block/user-block.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { LoginService } from 'src/app/auth/services/loging.service';
 
 @Component({
@@ -12,7 +13,8 @@ export class UserBlockComponent {
   constructor(
     private iconRegistry: MatIconRegistry,
     private sanitizer: DomSanitizer,
-    public logService: LoginService
+    public logService: LoginService,
+    private router: Router
   ) {
     iconRegistry.addSvgIcon(
       'user-icon',
@@ -20,9 +22,16 @@ export class UserBlockComponent {
         '../../../assets/svg/user-logo1.svg'
       )
     );
+    this.isLogged = logService.isLogged;
+    logService.loggedObserver.subscribe(
+      (isLogged) => (this.isLogged = isLogged)
+    );
   }
-
+  isLogged: boolean;
   onLogOut() {
     this.logService.logOut();
+  }
+  onLoginClick() {
+    this.router.navigateByUrl('auth');
   }
 }
