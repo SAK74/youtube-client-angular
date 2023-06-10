1129a104569bf3c96d9aa978df541213b6c24358
SAK74 <sergijks@gmail.com>
2023-06-10 14:17:03 +0200

    fix: filtering pipe

--- src/app/youtube/pipes/filtering.pippe.ts
+++ src/app/youtube/pipes/filtering.pippe.ts
@@ -5,10 +5,9 @@ import { SortType } from '../services/search-sort.model';
 @Pipe({ name: 'byWord' })
 export class FilterByWordPipe implements PipeTransform {
   transform(value: ItemType[], word: string) {
-    console.log('by word: ', value, word);
     return !word
       ? value
-      : value.filter((item) =>
+      : [...value].filter((item) =>
           item.snippet.title
             .toLocaleLowerCase()
             .includes(word.toLocaleLowerCase())
@@ -19,10 +18,9 @@ export class FilterByWordPipe implements PipeTransform {
 @Pipe({ name: 'byDate' })
 export class FilterByDatePipe implements PipeTransform {
   transform(value: ItemType[], dir: SortType) {
-    console.log('by date: ', value, dir);
     return !dir
       ? value
-      : value.sort((a, b) => {
+      : [...value].sort((a, b) => {
           const exp =
             new Date(a.snippet.publishedAt).getTime() -
             new Date(b.snippet.publishedAt).getTime();
@@ -34,10 +32,9 @@ export class FilterByDatePipe implements PipeTransform {
 @Pipe({ name: 'byViews' })
 export class FilterByViewsPipe implements PipeTransform {
   transform(value: ItemType[], dir: SortType) {
-    console.log('by views: ', value, dir);
     return !dir
       ? value
-      : value.sort((a, b) => {
+      : [...value].sort((a, b) => {
           const exp =
             Number(a.statistics.viewCount) - Number(b.statistics.viewCount);
           return dir === 'dec' ? exp : -exp;
